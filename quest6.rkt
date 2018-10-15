232f8b38adefd328d1d4cca1695b27deec02c89a
srfoster <stephen@thoughtstem.com>
2018-10-15 14:03:21 -0700

    worksheets for house placement

--- quest6.rkt
+++ quest6.rkt
@@ -3,7 +3,74 @@
 (provide quest6)
 
 (require ts-curric-common)
+
+(require (prefix-in p: pict/code))
+(require (prefix-in p: pict))
+
 (require "./common.rkt")
+
+
+(define (code+hint full-code
+                   hint-targets+hint-text)
+
+
+  
+  (define hint-targets (take hint-targets+hint-text
+                             (sub1 (length hint-targets+hint-text))))
+  
+
+  (define hint-text   (last   hint-targets+hint-text))
+  (define hint (p:text hint-text))
+
+  (define combined
+    (p:hc-append 50 full-code (p:colorize hint "red")))
+
+  (define (render-arrow target)
+   (p:pin-arrow-line 10
+                     combined 
+                     hint       p:lc-find
+                     target     p:rc-find
+                     #:line-width 3
+                     #:color "red"  ))
+
+  (define imgs
+    (map render-arrow
+         hint-targets))
+
+  (apply p:cc-superimpose imgs))
+
+(define (code+hints code . hints)
+  (p:lc-superimpose
+   (code+hint code
+              (first hints))
+   (code+hint code
+              (second hints))
+   )
+  )
+
+(define (code-blank (w 100))
+  (p:colorize (p:rectangle w 20) "red"))
+
+
+(define (house-placer-worksheet)
+  (define house-type (code-blank))
+  (define p-x  (code-blank))
+  (define p-y  (code-blank))
+  (define bg-n (code-blank))
+  
+  (define code 
+    (p:code ( #,house-type
+              (posn #,p-x #,p-y)
+              #:component
+              (active-on-bg #,bg-n))))
+
+  (p:scale
+   (code+hints code
+               (list house-type "wood-house or stone-house")
+               (list p-x p-y bg-n "number"))
+   2))
+
+(house-placer-worksheet)
 
 ;TODO: Change this.  It's copied from quest4...
 (define day6-2dgame
